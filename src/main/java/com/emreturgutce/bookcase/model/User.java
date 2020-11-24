61b632e8fd234cfb8bc4019a395e397531e59a78
emreturgutce <emrebursalee@gmail.com>
2020-11-24 20:16:00 +0300

    user model added

---
+++ src/main/java/com/emreturgutce/bookcase/model/User.java
@@ -0,0 +1,61 @@
+package com.emreturgutce.bookcase.model;
+
+import java.sql.Timestamp;
+
+public class User {
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private final Long created_at;
+    private final Long updated_at;
+
+    public User(String id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.created_at = (new Timestamp(System.currentTimeMillis())).getTime();
+        this.updated_at = (new Timestamp(System.currentTimeMillis())).getTime();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getCreated_at() {
+        return created_at;
+    }
+
+    public Long getUpdated_at() {
+        return updated_at;
+    }
+}
