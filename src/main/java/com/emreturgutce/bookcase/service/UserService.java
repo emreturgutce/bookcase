b6bfa20962489556844d160e28b728a6dd506f15
emreturgutce <emrebursalee@gmail.com>
2020-11-25 17:33:23 +0300

    findAll method added

--- src/main/java/com/emreturgutce/bookcase/service/UserService.java
+++ src/main/java/com/emreturgutce/bookcase/service/UserService.java
@@ -2,6 +2,10 @@ package com.emreturgutce.bookcase.service;
 
 import com.emreturgutce.bookcase.model.User;
 
+import java.util.List;
+
 public interface UserService {
     User create(String name, String email, String password) throws Exception;
+
+    List<User> findAll() throws Exception;
 }
--- src/main/java/com/emreturgutce/bookcase/service/UserServiceImpl.java
+++ src/main/java/com/emreturgutce/bookcase/service/UserServiceImpl.java
@@ -2,22 +2,31 @@ package com.emreturgutce.bookcase.service;
 
 import com.emreturgutce.bookcase.model.User;
 import com.emreturgutce.bookcase.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
-    @Autowired
+    final
     UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public User create(String name, String email, String password) throws Exception {
         UUID userId = userRepository.create(name, email, password);
 
         return userRepository.findById(userId);
+    }
+
+    @Override
+    public List<User> findAll() throws Exception {
+        return userRepository.findAll();
     }
 }
