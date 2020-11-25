b2b5054a84cc55df81337acd7b6800237ebd2098
emreturgutce <emrebursalee@gmail.com>
2020-11-25 17:34:03 +0300

    findAll route added

--- src/main/java/com/emreturgutce/bookcase/controller/UserController.java
+++ src/main/java/com/emreturgutce/bookcase/controller/UserController.java
@@ -2,24 +2,25 @@ package com.emreturgutce.bookcase.controller;
 
 import com.emreturgutce.bookcase.model.User;
 import com.emreturgutce.bookcase.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    @Autowired
+    final
     UserService userService;
 
-    @PostMapping("/create")
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("")
     public ResponseEntity<Map<String, String>> createUser(@RequestBody Map<String, Object> userMap) throws Exception {
         String name = (String) userMap.get("name");
         String email = (String) userMap.get("email");
@@ -30,6 +31,17 @@ public class UserController {
         Map<String, String> map = new HashMap<>();
 
         map.put("name", user.getName());
+
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Map<String, List<User>>> findAllUsers() throws Exception {
+        List<User> users = userService.findAll();
+
+        Map<String, List<User>> map = new HashMap<>();
+
+        map.put("users", users);
 
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
