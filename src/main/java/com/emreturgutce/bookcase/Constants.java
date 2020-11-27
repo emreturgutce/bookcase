78ee7b105ab87f8b711bee6d7ded127cb3c871fe
emreturgutce <emrebursalee@gmail.com>
2020-11-27 10:34:45 +0300

    some constants for token

--- src/main/java/com/emreturgutce/bookcase/Constants.java
+++ src/main/java/com/emreturgutce/bookcase/Constants.java
@@ -1,2 +1,7 @@
-package com.emreturgutce.bookcase;public class Constants {
+package com.emreturgutce.bookcase;
+
+public class Constants {
+    public static final String API_SECRET_KEY = "38324e708d82a8b02eab5a3b97d0caa49131ef74b36348d527da222014d597dd";
+
+    public static final long TOKEN_EXPIRATION_TIME = 2 * 60 * 60 * 1000;
 }
