585b47d028ae44fead900eecd498750450e225ae
emreturgutce <emrebursalee@gmail.com>
2020-11-28 13:21:23 +0300

    book repository interface added

--- src/main/java/com/emreturgutce/bookcase/repository/BookRepository.java
+++ src/main/java/com/emreturgutce/bookcase/repository/BookRepository.java
@@ -1,2 +1,7 @@
-package com.emreturgutce.bookcase.repository;public interface BookRepository {
+package com.emreturgutce.bookcase.repository;
+
+import java.util.UUID;
+
+public interface BookRepository {
+    UUID create(String name, UUID author_id);
 }
---
+++ src/main/java/com/emreturgutce/bookcase/service/BookService.java
@@ -0,0 +1,2 @@
+package com.emreturgutce.bookcase.service;public interface BookService {
+}
