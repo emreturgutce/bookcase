bb0bcb83deffd4e132ad589e512d803e15981174
emreturgutce <emrebursalee@gmail.com>
2020-11-28 13:47:27 +0300

    book service implemented

--- src/main/java/com/emreturgutce/bookcase/service/BookServiceImpl.java
+++ src/main/java/com/emreturgutce/bookcase/service/BookServiceImpl.java
@@ -1,2 +1,23 @@
-package com.emreturgutce.bookcase.service;public class BookServiceImpl {
+package com.emreturgutce.bookcase.service;
+
+import com.emreturgutce.bookcase.model.Book;
+import com.emreturgutce.bookcase.repository.BookRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@Transactional
+public class BookServiceImpl implements BookService {
+    final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) { this.bookRepository = bookRepository; }
+
+    @Override
+    public Book create(String name, String author_id) {
+        UUID bookId = bookRepository.create(name, author_id);
+
+        return bookRepository.findById(bookId);
+    }
 }
