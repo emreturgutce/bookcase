c4b6edebd06c03239922a01025dcced7f349fa18
emreturgutce <emrebursalee@gmail.com>
2020-11-28 13:46:56 +0300

    author_id param changed to string

--- src/main/java/com/emreturgutce/bookcase/service/BookService.java
+++ src/main/java/com/emreturgutce/bookcase/service/BookService.java
@@ -2,8 +2,6 @@ package com.emreturgutce.bookcase.service;
 
 import com.emreturgutce.bookcase.model.Book;
 
-import java.util.UUID;
-
 public interface BookService {
-    Book create(String name, UUID author_id);
+    Book create(String name, String author_id);
 }
