27d03719cbfd1fc29f7681f5cc9178819cc4077f
emreturgutce <emrebursalee@gmail.com>
2020-11-28 13:22:21 +0300

    book service interface added

--- src/main/java/com/emreturgutce/bookcase/service/BookService.java
+++ src/main/java/com/emreturgutce/bookcase/service/BookService.java
@@ -1,2 +1,9 @@
-package com.emreturgutce.bookcase.service;public interface BookService {
+package com.emreturgutce.bookcase.service;
+
+import com.emreturgutce.bookcase.model.Book;
+
+import java.util.UUID;
+
+public interface BookService {
+    Book create(String name, UUID author_id);
 }
