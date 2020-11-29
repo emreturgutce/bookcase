d39d8914a62fca3eac66f2e5097a60273f803026
emreturgutce <emrebursalee@gmail.com>
2020-11-29 10:01:18 +0300

    findAll method added to BookService Interface

--- src/main/java/com/emreturgutce/bookcase/service/BookService.java
+++ src/main/java/com/emreturgutce/bookcase/service/BookService.java
@@ -2,6 +2,10 @@ package com.emreturgutce.bookcase.service;
 
 import com.emreturgutce.bookcase.model.Book;
 
+import java.util.List;
+
 public interface BookService {
     Book create(String name, String author_id);
+
+    List<Book> findAll();
 }
