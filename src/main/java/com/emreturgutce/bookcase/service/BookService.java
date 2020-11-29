3ad1796978a793f37d9142a6136461a0c1228079
emreturgutce <emrebursalee@gmail.com>
2020-11-29 12:21:12 +0300

    findById method added

--- src/main/java/com/emreturgutce/bookcase/service/BookService.java
+++ src/main/java/com/emreturgutce/bookcase/service/BookService.java
@@ -3,9 +3,12 @@ package com.emreturgutce.bookcase.service;
 import com.emreturgutce.bookcase.model.Book;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface BookService {
     Book create(String name, String author_id);
+
+    Book findById(UUID id);
 
     List<Book> findAll();
 }
