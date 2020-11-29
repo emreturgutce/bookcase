8a58c3f6e48c2bc8874138712cf6b0cf2cf858c2
emreturgutce <emrebursalee@gmail.com>
2020-11-29 10:00:39 +0300

    findAll method added to BookRepository Interface

--- src/main/java/com/emreturgutce/bookcase/repository/BookRepository.java
+++ src/main/java/com/emreturgutce/bookcase/repository/BookRepository.java
@@ -2,10 +2,13 @@ package com.emreturgutce.bookcase.repository;
 
 import com.emreturgutce.bookcase.model.Book;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BookRepository {
     UUID create(String name, String author_id);
 
     Book findById(UUID id);
+
+    List<Book> findAll();
 }
