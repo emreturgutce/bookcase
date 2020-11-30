3d80493a7ab850219750d43a2ff8901e192baaf1
emreturgutce <emrebursalee@gmail.com>
2020-11-30 10:31:20 +0300

    users_books interface created and create method added

--- src/main/java/com/emreturgutce/bookcase/repository/Users_BooksRepository.java
+++ src/main/java/com/emreturgutce/bookcase/repository/Users_BooksRepository.java
@@ -1,2 +1,7 @@
-package com.emreturgutce.bookcase.repository;public interface Users_BooksRepository {
+package com.emreturgutce.bookcase.repository;
+
+import java.util.UUID;
+
+public interface Users_BooksRepository {
+    UUID create(UUID user_id, UUID book_id);
 }
