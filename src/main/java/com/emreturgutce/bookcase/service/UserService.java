7a9fad21898c840ea6068fe6fb49882a574cd716
emreturgutce <emrebursalee@gmail.com>
2020-11-30 22:00:25 +0300

    user service jpa refactored

--- src/main/java/com/emreturgutce/bookcase/service/UserService.java
+++ src/main/java/com/emreturgutce/bookcase/service/UserService.java
@@ -4,12 +4,8 @@ import com.emreturgutce.bookcase.exception.BadRequestException;
 import com.emreturgutce.bookcase.exception.UnauthorizedException;
 import com.emreturgutce.bookcase.model.User;
 
-import java.util.List;
-
 public interface UserService {
     User signup(String name, String email, String password) throws BadRequestException;
 
     User login(String email, String password) throws UnauthorizedException;
-
-    List<User> findAll();
 }
