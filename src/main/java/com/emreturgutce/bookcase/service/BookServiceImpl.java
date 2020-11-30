66e9ebac7f08640520525e659d957954e3c27abb
emreturgutce <emrebursalee@gmail.com>
2020-11-30 22:17:26 +0300

    book service implementation create and findById methods refactored to jpa

--- src/main/java/com/emreturgutce/bookcase/service/BookServiceImpl.java
+++ src/main/java/com/emreturgutce/bookcase/service/BookServiceImpl.java
@@ -3,7 +3,10 @@ package com.emreturgutce.bookcase.service;
 import com.emreturgutce.bookcase.exception.BadRequestException;
 import com.emreturgutce.bookcase.exception.NotFoundException;
 import com.emreturgutce.bookcase.model.Book;
+import com.emreturgutce.bookcase.model.User;
 import com.emreturgutce.bookcase.repository.BookRepository;
+import com.emreturgutce.bookcase.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,17 +15,23 @@ import java.util.UUID;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
     final BookRepository bookRepository;
-
-    public BookServiceImpl(BookRepository bookRepository) { this.bookRepository = bookRepository; }
+    final UserRepository userRepository;
 
     @Override
-    public Book create(String name, String author_id) throws BadRequestException{
+    public Book create(String name, UUID author_id) throws BadRequestException{
         try {
-            UUID bookId = bookRepository.create(name, author_id);
+            User author = userRepository.getOne(author_id);
 
-            return bookRepository.findById(bookId);
+            Book book = new Book();
+            book.setName(name);
+            book.setAuthor(author);
+
+            bookRepository.save(book);
+
+            return book;
         } catch (Exception e) {
             throw new BadRequestException("Invalid request");
         }
@@ -31,33 +40,33 @@ public class BookServiceImpl implements BookService {
     @Override
     public Book findById(UUID id) throws NotFoundException {
         try {
-            return bookRepository.findById(id);
+            return bookRepository.findById(id).get();
         } catch (Exception e) {
             throw new NotFoundException("Book not found with the given id");
         }
 
     }
-
-    @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
-    }
-
-    @Override
-    public void update(UUID id, Book book) throws BadRequestException {
-        try {
-            bookRepository.update(id, book.getName(), UUID.fromString(book.getAuthor_id()));
-        } catch (Exception e) {
-            throw new BadRequestException("invalid request");
-        }
-    }
-
-    @Override
-    public void delete(UUID id) throws NotFoundException {
-        try {
-            bookRepository.delete(id);
-        } catch (Exception e) {
-            throw new NotFoundException("Book not found");
-        }
-    }
+//
+//    @Override
+//    public List<Book> findAll() {
+//        return bookRepository.findAll();
+//    }
+//
+//    @Override
+//    public void update(UUID id, Book book) throws BadRequestException {
+//        try {
+//            bookRepository.update(id, book.getName(), UUID.fromString(book.getAuthor_id()));
+//        } catch (Exception e) {
+//            throw new BadRequestException("invalid request");
+//        }
+//    }
+//
+//    @Override
+//    public void delete(UUID id) throws NotFoundException {
+//        try {
+//            bookRepository.delete(id);
+//        } catch (Exception e) {
+//            throw new NotFoundException("Book not found");
+//        }
+//    }
 }
