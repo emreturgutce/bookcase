4c9df809a60e4f3bd47e5606177797fc047904cb
emreturgutce <emrebursalee@gmail.com>
2020-11-30 20:04:46 +0300

    book model refactor

--- src/main/java/com/emreturgutce/bookcase/model/Book.java
+++ src/main/java/com/emreturgutce/bookcase/model/Book.java
@@ -1,49 +1,38 @@
 package com.emreturgutce.bookcase.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Book {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
     private String id;
+
+    @Column(unique = true, nullable = false)
     private String name;
-    private String author_id;
-    private final String created_at;
-    private final String updated_at;
 
-    public Book(String id, String name, String author_id, String created_at, String updated_at) {
-        this.id = id;
-        this.name = name;
-        this.author_id = author_id;
-        this.created_at = created_at;
-        this.updated_at = updated_at;
-    }
+    @ManyToOne
+    @JoinColumn
+    private User author;
 
-    public String getId() {
-        return id;
-    }
+    @CreationTimestamp
+    private LocalDateTime created_at;
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAuthor_id() {
-        return author_id;
-    }
-
-    public void setAuthor_id(String author_id) {
-        this.author_id = author_id;
-    }
-
-    public String getCreated_at() {
-        return created_at;
-    }
-
-    public String getUpdated_at() {
-        return updated_at;
-    }
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
 }
