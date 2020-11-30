8282d29b1f59ba6d3912d7c4bd2aeb22d488ed3e
emreturgutce <emrebursalee@gmail.com>
2020-11-30 21:54:31 +0300

    findByEmail method added to user repository interface

--- src/main/java/com/emreturgutce/bookcase/repository/UserRepository.java
+++ src/main/java/com/emreturgutce/bookcase/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.emreturgutce.bookcase.repository;
 
 import com.emreturgutce.bookcase.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {}
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByEmail(String email);
+}
