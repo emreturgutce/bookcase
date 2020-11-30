9411e410eea69675bb03ba4815f38f28cc7b973d
emreturgutce <emrebursalee@gmail.com>
2020-11-30 22:16:47 +0300

    create and findById methods refactored to jpa

--- src/main/java/com/emreturgutce/bookcase/service/BookService.java
+++ src/main/java/com/emreturgutce/bookcase/service/BookService.java
@@ -8,13 +8,13 @@ import java.util.List;
 import java.util.UUID;
 
 public interface BookService {
-    Book create(String name, String author_id) throws BadRequestException;
+    Book create(String name, UUID author_id) throws BadRequestException;
 
     Book findById(UUID id) throws NotFoundException;
-
-    List<Book> findAll();
-
-    void update(UUID id, Book book) throws BadRequestException;
-
-    void delete(UUID id) throws NotFoundException;
+//
+//    List<Book> findAll();
+//
+//    void update(UUID id, Book book) throws BadRequestException;
+//
+//    void delete(UUID id) throws NotFoundException;
 }
