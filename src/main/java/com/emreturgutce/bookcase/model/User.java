0013e95b0d82a6f9faf44b861335e61a052dc21e
emreturgutce <emrebursalee@gmail.com>
2020-11-30 19:59:12 +0300

    usre model refactor to jpa

--- src/main/java/com/emreturgutce/bookcase/model/User.java
+++ src/main/java/com/emreturgutce/bookcase/model/User.java
@@ -1,59 +1,40 @@
 package com.emreturgutce.bookcase.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
 public class User {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
     private String id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
-    private final String created_at;
-    private final String updated_at;
 
-    public User(String id, String name, String email, String password, String created_at, String updated_at) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.created_at = created_at;
-        this.updated_at = updated_at;
-    }
+    @CreationTimestamp
+    private LocalDateTime created_at;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getCreated_at() {
-        return created_at;
-    }
-
-    public String getUpdated_at() {
-        return updated_at;
-    }
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
 }
