e87827fafb50f27d203011ccc9049b973e89adf0
emreturgutce <emrebursalee@gmail.com>
2020-11-30 22:17:50 +0300

    create book and findById routes refactored to jpa

--- src/main/java/com/emreturgutce/bookcase/controller/BookController.java
+++ src/main/java/com/emreturgutce/bookcase/controller/BookController.java
@@ -2,7 +2,7 @@ package com.emreturgutce.bookcase.controller;
 
 import com.emreturgutce.bookcase.model.Book;
 import com.emreturgutce.bookcase.service.BookService;
-import com.emreturgutce.bookcase.service.Users_BooksService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,74 +15,65 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/books")
+@RequiredArgsConstructor
 public class BookController {
-    final BookService bookService;
-    final Users_BooksService users_booksService;
+    private final BookService bookService;
 
-    public BookController(BookService bookService, Users_BooksService users_booksService) {
-        this.bookService = bookService;
-        this.users_booksService = users_booksService;
-    }
-
-    @PostMapping("")
+    @PostMapping
     public ResponseEntity<Map<String, String>> createBook(@RequestBody Map<String, Object> bookMap) {
         String name = (String) bookMap.get("name");
-        String author_id = (String) bookMap.get("author_id");
+        UUID author_id = UUID.fromString(bookMap.get("author_id").toString());
 
         Book book = bookService.create(name, author_id);
 
         Map<String, String> map = new HashMap<>();
 
-        map.put("id", book.getId());
         map.put("name", book.getName());
-        map.put("author_id", book.getAuthor_id());
-        map.put("created_at", book.getCreated_at());
-        map.put("updated_at", book.getUpdated_at());
 
         return new ResponseEntity<>(map, HttpStatus.CREATED);
     }
-
-    @GetMapping("")
-    public ResponseEntity<Map<String, List<Book>>> findAllBooks()  {
-        List<Book> books = bookService.findAll();
-
-        Map<String, List<Book>> map = new HashMap<>();
-
-        map.put("books", books);
-
-        return new ResponseEntity<>(map, HttpStatus.OK);
-    }
+//
+//    @GetMapping("")
+//    public ResponseEntity<Map<String, List<Book>>> findAllBooks()  {
+//        List<Book> books = bookService.findAll();
+//
+//        Map<String, List<Book>> map = new HashMap<>();
+//
+//        map.put("books", books);
+//
+//        return new ResponseEntity<>(map, HttpStatus.OK);
+//    }
 
     @GetMapping("/{bookId}")
-    public ResponseEntity<Map<String, Book>> findBookById(@PathVariable("bookId") UUID bookId) {
+    public ResponseEntity<Map<String, String>> findBookById(@PathVariable("bookId") UUID bookId) {
         Book book = bookService.findById(bookId);
 
-        Map<String, Book> map = new HashMap<>();
+        Map<String, String> map = new HashMap<>();
 
-        map.put("book", book);
+        map.put("name", book.getName());
 
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
-    @PutMapping("/{bookId}")
-    public ResponseEntity<HttpStatus> updateBook(@PathVariable("bookId") UUID bookId, @RequestBody Book book) {
-        bookService.update(bookId, book);
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{bookId}")
-    public ResponseEntity<HttpStatus> deleteBook(@PathVariable("bookId") UUID bookId) {
-        bookService.delete(bookId);
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @GetMapping("/{bookId}/add")
-    public ResponseEntity<HttpStatus> addBookToFavorites(HttpServletRequest request, @PathVariable("bookId") UUID bookId) {
-        UUID userId = UUID.fromString(request.getAttribute("id").toString());
-        users_booksService.create(userId, bookId);
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+//    @PutMapping("/{bookId}")
+//    public ResponseEntity<HttpStatus> updateBook(@PathVariable("bookId") UUID bookId, @RequestBody Book book) {
+//        bookService.update(bookId, book);
+//
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
+//
+//    @DeleteMapping("/{bookId}")
+//    public ResponseEntity<HttpStatus> deleteBook(@PathVariable("bookId") UUID bookId) {
+//        bookService.delete(bookId);
+//
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/{bookId}/add")
+//    public ResponseEntity<HttpStatus> addBookToFavorites(HttpServletRequest request, @PathVariable("bookId") UUID bookId) {
+//        UUID userId = UUID.fromString(request.getAttribute("id").toString());
+//        users_booksService.create(userId, bookId);
+//
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
 }
