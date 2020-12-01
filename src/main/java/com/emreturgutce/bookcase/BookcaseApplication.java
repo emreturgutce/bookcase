dfd7b961a3dc42123308c2b786546f25c3d63ba2
emreturgutce <emrebursalee@gmail.com>
2020-12-01 13:48:28 +0300

    cors added

--- src/main/java/com/emreturgutce/bookcase/BookcaseApplication.java
+++ src/main/java/com/emreturgutce/bookcase/BookcaseApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class BookcaseApplication {
@@ -24,5 +27,18 @@ public class BookcaseApplication {
 		registrationBean.addUrlPatterns("/api/books/*");
 
 		return registrationBean;
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedOrigin("*");
+		configuration.addAllowedHeader("*");
+		source.registerCorsConfiguration("/**", configuration);
+		filterRegistrationBean.setFilter(new CorsFilter(source));
+		filterRegistrationBean.setOrder(0);
+		return filterRegistrationBean;
 	}
 }
