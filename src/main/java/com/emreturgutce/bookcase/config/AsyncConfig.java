519e27d6025db60db3f5bc5ec56557e75e9973d1
emreturgutce <emrebursalee@gmail.com>
2020-12-03 09:01:49 +0300

    async config added

--- src/main/java/com/emreturgutce/bookcase/config/AsyncConfig.java
+++ src/main/java/com/emreturgutce/bookcase/config/AsyncConfig.java
@@ -1,2 +1,24 @@
-package com.emreturgutce.bookcase.config;public class AsyncConfig {
+package com.emreturgutce.bookcase.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("BookcaseThread-");
+        executor.initialize();
+        return executor;
+    }
 }
